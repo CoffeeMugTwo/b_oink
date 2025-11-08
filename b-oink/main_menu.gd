b158b56e05809cabd1279eeb7edd7661b4ff4a85
pgehl99 <philipp.gehl99@gmail.com>
2025-11-08 17:36:19 +0100

    Changed Resolution, Connected PLlay Button to world

--- b-oink/main_menu.gd
+++ b-oink/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/TEst_Art.tscn")
+	get_tree().change_scene_to_file("res://Scenes/World.tscn")
 
 
 func _on_settings_button_pressed() -> void:
