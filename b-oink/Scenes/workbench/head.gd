7766ee6771e18eab68de0459ea7de5d2fc15554c
thomas.kittler <thomas.kittler.01@gmail.com>
2025-11-08 20:25:34 +0100

    add grad fnc

--- b-oink/Scenes/workbench/head.gd
+++ b-oink/Scenes/workbench/head.gd
@@ -7,6 +7,27 @@ const JUMP_VELOCITY = 4.5
 @export var spring_force : Vector3 = Vector3(0, 0 ,0)
 @export var max_input_force_scalar : float = 10.0
 @export var mass : float = 1.0
+@export var jump_force_scalar : float = 600
+@export var max_grab_distance : float = 0.1
+
+func _can_grab() -> bool:
+	var space_state = get_world_3d().direct_space_state
+	var sphere = SphereShape3D.new()
+	sphere.radius = max_grab_distance
+	var query = PhysicsShapeQueryParameters3D.new()
+	query.shape = sphere
+	query.transform.origin = global_position
+	query.collide_with_areas = false
+	query.collide_with_bodies = true
+	
+	var results = space_state.intersect_shape(query, 1)
+	for r in results:
+		var collider = r["collider"]
+		if collider is StaticBody3D:
+			return true
+			
+	return false
+
 
 func _update_velocity(delta: float) -> void:
 	# update velocity according to resulting force 
@@ -18,12 +39,14 @@ func _update_velocity(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
 		current_force_vector += Vector3(-1, 0, 0) * max_input_force_scalar
 		
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		current_force_vector += Vector3(0, 1, 0) * jump_force_scalar
+		
+		
 	var current_acceleration = current_force_vector / mass
 	
 	velocity += current_acceleration * delta
-		
 	
-		 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -31,11 +54,11 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	_update_velocity(delta)
+	
+	# handle grap
+	if Input.is_action_pressed("p1_hold") and _can_grab():
+		velocity = Vector3(0, 0, 0)
 
 	position.z = 0
 	move_and_slide()
