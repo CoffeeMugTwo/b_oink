1658a0248ae789ad199d7f5086d6eb60c2cabcf6
pgehl99 <philipp.gehl99@gmail.com>
2025-11-09 13:10:10 +0100

    characters_new with spring scaling

--- b-oink/Scripts/characters.gd
+++ b-oink/Scripts/characters.gd
@@ -1,24 +1,31 @@
 extends Node3D
 
-
 @onready var player_back: Node3D = $PlayerBack
 @onready var player_head: Node3D = $PlayerHead
 @onready var spring: Node3D = $Spring
 
 func _process(_delta):
-	var pos_a = player_back.global_position
-	var pos_b = player_head.global_position
-
+	var pos_a: Vector3 = player_back.global_position
+	var pos_b: Vector3 = player_head.global_position
+	print("characters script: ", pos_a)
 	# Midpoint between the two players
 	spring.global_position = (pos_a + pos_b) * 0.5
 
-	# Vector from A → B
-	var dir = pos_b - pos_a
-	dir.z = 0  # ignore Z for 2.5D
+	# Direction vector A → B
+	var dir: Vector3 = pos_b - pos_a
+	dir.z = 0  # lock to 2.5D plane (XY)
 
-	# Rotate around Z axis (so it points correctly in 2.5D)
-	spring.rotation = Vector3(0, 0, atan2(dir.y, dir.x))
+	# Compute rotation angle in radians
+	var angle = atan2(dir.y, dir.x)
 
-	# Stretch along local X axis
+	# Apply rotation (make sure your spring’s forward axis is +X)
+	spring.rotation = Vector3(0, 0, angle)
+
+	# Stretch spring along X axis based on distance
 	var distance = dir.length()
-	spring.scale = Vector3(distance, 1, 1)
+
+	# Store original scale (so it doesn't distort over time)
+	var base_scale = Vector3(1, 1, 1)
+
+	# Apply stretch only on X axis
+	spring.scale = Vector3(distance, base_scale.y, base_scale.z)
---
+++ b-oink/Scripts/characters_new.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+@onready var player_back: Node3D = $PlayerBack
+@onready var player_head: Node3D = $PlayerHead
+@onready var spring: Node3D = $Spring
+
+func _process(_delta):
+	var pos_a = player_back.global_position
+	var pos_b = player_head.global_position
+	pos_a.z = 0
+	pos_b.z = 0
+
+	# Midpoint
+	spring.global_position = (pos_a + pos_b) * 0.5
+
+	# Rotate spring to face head
+	spring.look_at(pos_b, Vector3.UP)
+
+	# Distance
+	var distance = (pos_b - pos_a).length()
+
+	# Scale along local X (assuming spring is modeled along X)
+	print(spring.scale)
+	var s = spring.scale
+	s.z = distance * 0.5 * 0.5/0.3   # 0.5 if pivot is centered
+	spring.scale = s
