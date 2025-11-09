7490630539a010117f73c29a9bc11d34fe71e220
pgehl99 <philipp.gehl99@gmail.com>
2025-11-09 13:46:14 +0100

    Character changes

--- b-oink/Scenes/workbench/spring_test_code.gd
+++ b-oink/Scenes/workbench/spring_test_code.gd
@@ -1,13 +1,25 @@
 extends Node3D
 
+@onready var player_back: Node3D = $butt/butt_anker
+@onready var player_head: Node3D = $head/head_anker
+@onready var spring: Node3D = $Spring
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _process(_delta):
+	var pos_a = player_back.global_position
+	var pos_b = player_head.global_position
+	pos_a.z = 0
+	pos_b.z = 0
 
+	# Midpoint
+	spring.global_position = (pos_a + pos_b) * 0.5
+	# Rotate spring to face head
+	spring.look_at(pos_b, Vector3.UP)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-	
+	# Distance
+	var distance = (pos_b - pos_a).length()
+
+	# Scale along local X (assuming spring is modeled along X)
+	print(spring.scale)
+	var s = spring.scale
+	s.z = distance * 0.5 * 0.5/0.3   # 0.5 if pivot is centered
+	spring.scale = s
